5b03edd8c315537531967cd7f4224d177236885e
katrinundfritz <fritzkatrin@gmail.com>
2018-03-02 22:52:11 +0100

    3.9 Optimized routes.rb after feedback from Marco

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'simple_pages/about'
-
-  get 'simple_pages/contact'
-
-  get 'simple_pages/index'
   root 'simple_pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+  get 'simple_pages/index'
 end
