d6e66c9a0896a93033e753a86ac3c628e4ae4ff9
katrinundfritz <fritzkatrin@gmail.com>
2018-05-04 08:07:07 +0200

    Remove old product variable declaration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,6 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    @comments = @product.comments.order("created_at DESC")
     @comments = Comment.paginate(:page => params[:page], :per_page => 5).order('created_at DESC')
   end
 
