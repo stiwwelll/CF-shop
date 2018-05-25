5d1f37a82ac6849421718335d193bef1695d4213
katrinundfritz <fritzkatrin@gmail.com>
2018-05-25 09:52:53 +0200

    6.6 Create order on payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,11 +9,12 @@ class PaymentsController < ApplicationController
         amount: (@product.price * 100),
         currency: "eur",
         source: token,
-        description: params[:stripeEmail]
+        description: params[:stripeEmail],
+        receipt_email: params[:stripeEmail]
       )
 
     if charge.paid
-     Order.create(:product_id, :user_id, :total)
+     Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
     end
 
     rescue Stripe::CardError => e
